1ddc213a7fdffb897ac59204aea81b9d22e06b35
jakep13 <jakepogharian@MacBook-Pro.local>
2020-10-03 17:08:29 -0400

    fixed some stuff, gradle build works better now

--- project-group-13-Backend/src/main/java/ca/mcgill/ecse321/projectgroup13/ProjectGroup13Application.java
+++ project-group-13-Backend/src/main/java/ca/mcgill/ecse321/projectgroup13/ProjectGroup13Application.java
@@ -1,9 +1,13 @@
 package ca.mcgill.ecse321.projectgroup13;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@EnableJpaRepositories
 public class ProjectGroup13Application {
 
 	public static void main(String[] args) {
--- src/main/java/application/GalleryApp.java
+++ src/main/java/application/GalleryApp.java
@@ -1,11 +1,21 @@
-package application;
+package ca.mcgill.ecse321.Application;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
+@SpringBootApplication
 public class GalleryApp {
 
-	public static void main(String[] args) {
-        
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GalleryApp.class, args);
+  }
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 
 }
-
