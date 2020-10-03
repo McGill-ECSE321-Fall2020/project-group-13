bdef45252ae39e9ed8e22e346f32524dcc32f85e
jakep13 <jakepogharian@MacBook-Pro.local>
2020-10-02 22:55:51 -0400

    added ArtGallery-backend, springboot implemented

---
+++ ArtGallery-backend/src/main/java/ca/mcgill/ecse321/projectgroup13/ProjectGroup13Application.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.projectgroup13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGroup13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGroup13Application.class, args);
+	}
+
+}
---
+++ ArtGallery-backend/src/test/java/ca/mcgill/ecse321/projectgroup13/ProjectGroup13ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.projectgroup13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGroup13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
