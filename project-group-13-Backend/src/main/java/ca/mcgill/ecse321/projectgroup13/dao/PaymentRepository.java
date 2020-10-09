d29ade9b6e2f9906ff7f40b4fab2914d2fb69aef
cesararnouk <arnoukcesar@gmail.com>
2020-10-08 23:58:22 -0400

    fixed TestPayment

--- project-group-13-Backend/src/main/java/ca/mcgill/ecse321/projectgroup13/dao/PaymentRepository.java
+++ project-group-13-Backend/src/main/java/ca/mcgill/ecse321/projectgroup13/dao/PaymentRepository.java
@@ -1,24 +1,21 @@
 package ca.mcgill.ecse321.projectgroup13.dao;
 
+import ca.mcgill.ecse321.projectgroup13.model.Order;
+import ca.mcgill.ecse321.projectgroup13.model.Payment;
+import org.springframework.data.repository.CrudRepository;
+
 import java.sql.Date;
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
-
-import ca.mcgill.ecse321.projectgroup13.model.Order;
-import ca.mcgill.ecse321.projectgroup13.model.Payment;
-
 public interface PaymentRepository extends CrudRepository<Payment, String> {
+
 	Payment findPaymentByPaymentID(String paymentID);
+	Payment findPaymentByOrder(Order order);
 	List<Payment> findByPaymentDateAfter(Date date);
 	List<Payment> findByPaymentDateBefore(Date date);
 	List<Payment> findByPaymentDateBetween(Date startDate, Date endDate);
 	List<Payment> findByCardNumber(double cardNumber);
 	List<Payment> findByTotalAmountGreaterThan(double value);
 	List<Payment> findByTotalAmountLessThan(double value);
-	
-	
-	
-	
-	
+
 }
