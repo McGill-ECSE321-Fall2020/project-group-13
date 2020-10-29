eefd4605b58d957352053058f42b065a645f7788
cesararnouk <arnoukcesar@gmail.com>
2020-10-28 23:45:25 -0400

    fixed artworkRepository from d1

--- project-group-13-Backend/src/main/java/ca/mcgill/ecse321/projectgroup13/dao/ArtworkRepository.java
+++ project-group-13-Backend/src/main/java/ca/mcgill/ecse321/projectgroup13/dao/ArtworkRepository.java
@@ -12,6 +12,7 @@ import java.util.Set;
 public interface ArtworkRepository extends CrudRepository<Artwork, Long>{
 
     Artwork findArtworkByTitle(String title);
+    Artwork findArtworkByArtist(User artist);
     Artwork findArtworkByArtworkID(String artworkID);
     List<Artwork> findByArtist(User Artist);
     //Set<Artwork> findArtworkByArtistUsername(Set<String> artistsID);
