b3d0700f0e0dd5284af8ffeff79a461506f3c4af
jakep13 <jakepogharian@MacBook-Pro.local>
2020-10-30 18:52:07 -0400

    issue 39 Added gradle dependencies initiated services

---
+++ project-group-13-Backend/src/test/java/ca/mcgill/ecse321/projectgroup13/services/TestServiceUser.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse321.projectgroup13.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+
+public class TestServiceUser {
+
+	
+}
