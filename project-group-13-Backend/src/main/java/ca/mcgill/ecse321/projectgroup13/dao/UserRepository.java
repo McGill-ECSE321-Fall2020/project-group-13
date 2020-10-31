6775899d0c99b3e0f5e912c8d26433e6251dcfd7
Runtime Learner <matthias15002@gmail.com>
2020-10-31 15:34:33 -0400

    fix UserRepository

--- project-group-13-Backend/src/main/java/ca/mcgill/ecse321/projectgroup13/dao/UserRepository.java
+++ project-group-13-Backend/src/main/java/ca/mcgill/ecse321/projectgroup13/dao/UserRepository.java
@@ -10,6 +10,6 @@ public interface UserRepository extends CrudRepository<User, String> {
 
     User findUserByUsername(String username);
     User findUserByEmail(String email);
-    User findUserById(int id);
+    //User findUserById(int id);
 
 }
