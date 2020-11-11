0e14fdbdb7ad3c95fca017b02c063032e0bed78c
Ibrahim T. Miraj <zerobottles@outlook.com>
2020-11-11 15:32:55 -0500

    fixed bugs in artworkDetails

--- project-group-13-frontend/src/components/ArtworkDetails.vue
+++ project-group-13-frontend/src/components/ArtworkDetails.vue
@@ -1,13 +1,12 @@
 <template>
     <div id="artworkdetails">
         <div class="left-column">
-            <img src="./artImage.jpg" alt="">
-            //add user section here
+             <img src="./artImage.jpg" alt="">
         </div>
         <div class="right-column">
-            <h2 class="title">{{ artwork.title }}</h2>
-            <p class="price">{{ artwork.price }}</p>
-            <p class="description">{{ artwork.description }}</p>
+            <h2 class="title">TITLE</h2>
+            <p class="price">3042948</p>
+            <p class="description">pocterfdu;foerhfe;foiefweofehverferhfe;fwiwe;fiwej</p>
         </div>
     </div>
 </template>
