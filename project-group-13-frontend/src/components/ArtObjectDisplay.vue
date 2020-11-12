03fe760ca3510e7a472f99816ef5b47547279a05
Ibrahim T. Miraj <zerobottles@outlook.com>
2020-11-11 20:26:16 -0500

    git commiting ui improvements

--- project-group-13-frontend/src/components/ArtObjectDisplay.vue
+++ project-group-13-frontend/src/components/ArtObjectDisplay.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="mt-2 pt-2">
     <b-container class="shadow-lg rounded">
-        <b-row class="max-w-5xl mx-auto px-6 pt-3">
+        <b-row class="max-w-5xl mx-auto px-6 pt-3 m-0">
             <div class=""><h4 class="d-inline">{{displayHeading}} | </h4><p class="d-inline"> See more &rarr;</p></div>
         </b-row>
         <b-row>
-            <div class="max-w-5xl mx-auto px-1 py-1 mx-0">
+            <div class="max-w-5xl mx-auto px-1 py-1 m-0">
     <div class="d-flex flex-wrap mx-0 mt-0 justify-content-center">
         <!-- The line below is for instantiating multiple artobjects -->
       <!-- <div
