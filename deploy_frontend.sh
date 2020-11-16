7239eaf39ec77eae9fd206625944a5442613e956
Runtime Learner <matthias15002@gmail.com>
2020-11-16 11:26:26 -0500

    fix merge conflict. Add cleanup command to remove heroku remote

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 git remote rm heroku
 heroku git:remote -a project-group-13-frontend
-<<<<<<< HEAD
-git push heroku master
-=======
 git subtree push --prefix project-group-13-frontend/ heroku master
->>>>>>> 6e128fff7d5846fc1dfc6f80de65149c782255af
+git remote rm heroku
