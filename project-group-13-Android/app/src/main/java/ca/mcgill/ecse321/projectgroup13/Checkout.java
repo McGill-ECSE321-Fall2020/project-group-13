f1fcd9bad50e1abc5816f89da44f93409816a6ad
Ibrahim T. Miraj <zerobottles@outlook.com>
2020-11-27 15:43:38 -0500

    created checkout page

---
+++ project-group-13-Android/app/src/main/java/ca/mcgill/ecse321/projectgroup13/Checkout.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.projectgroup13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Checkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+    }
+}
