ed590f210049f7b1b5ca334fb5a22a67010b736e
cesararnouk <arnoukcesar@gmail.com>
2020-11-27 17:19:04 -0500

    working on artwork page

--- project-group-13-Android/app/src/main/java/ca/mcgill/ecse321/projectgroup13/ArtworkDetail.java
+++ project-group-13-Android/app/src/main/java/ca/mcgill/ecse321/projectgroup13/ArtworkDetail.java
@@ -1,16 +1,28 @@
 package ca.mcgill.ecse321.projectgroup13;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ArtworkDetail extends AppCompatActivity {
+    private String error = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +39,51 @@ public class ArtworkDetail extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = getIntent();
+        int artId = intent.getIntExtra("artworkID", 0);
+
+        RequestParams params = new RequestParams();
+        String artworkUrl = "artwork/byId/" + artId;
+        HttpUtils.get(artworkUrl, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                try {
+                    String title = response.getJSONObject(0).getString("title");
+                    TextView tvTitle = (TextView) findViewById(R.id.title);
+                    tvTitle.setText(title);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                    error += "!!!!";
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                    error += "!!!";
+                }
+                refreshErrorMessage();
+            }
+        });
+
+    }
+
+    private void refreshErrorMessage() {
+        // set the error message
+        TextView tvError = (TextView) findViewById(R.id.error);
+        tvError.setText(error);
+
+        if (error == null || error.length() == 0) {
+            tvError.setVisibility(View.GONE);
+        } else {
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void buy(View v){
+
     }
 }
