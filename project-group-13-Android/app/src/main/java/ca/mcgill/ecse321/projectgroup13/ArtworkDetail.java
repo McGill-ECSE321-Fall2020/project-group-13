90faa33314f488ba381dc7b6373839e1f805ef1d
runtime-learner <matthias15002@gmail.com>
2020-11-27 21:31:57 -0500

    get rid of fab

--- project-group-13-Android/app/src/main/java/ca/mcgill/ecse321/projectgroup13/ArtworkDetail.java
+++ project-group-13-Android/app/src/main/java/ca/mcgill/ecse321/projectgroup13/ArtworkDetail.java
@@ -38,15 +38,6 @@ public class ArtworkDetail extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         Intent intent = getIntent();
         artId = intent.getIntExtra("artworkID", 0);
         System.out.println("The artid issss: " + artId);
