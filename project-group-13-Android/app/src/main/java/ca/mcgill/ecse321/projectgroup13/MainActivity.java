4363020b4ee4e9ead0ea611d43a87e8f76ee16a5
runtime-learner <matthias15002@gmail.com>
2020-11-27 21:19:21 -0500

    getting rid of useless files

--- project-group-13-Android/app/src/main/java/ca/mcgill/ecse321/projectgroup13/MainActivity.java
+++ project-group-13-Android/app/src/main/java/ca/mcgill/ecse321/projectgroup13/MainActivity.java
@@ -103,11 +103,6 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
 //        noinspection SimplifiableIfStatement
         if (id == R.id.action_login) {
             Intent loginPage = new Intent(MainActivity.this, Login.class);
